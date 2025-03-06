2024230efbccd863cde98e330b15b51e52b6bca3
ShouvikSarkar1 <shouviksarkarmusic@gmail.com>
2025-03-05 20:43:02 -0500

    update chat app

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,18 +1,52 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+let users = {}; // Store connected users
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('A user connected');
+
+    // Handle nickname assignment
+    socket.on('set_nickname', (nickname) => {
+        users[socket.id] = nickname;
+        io.emit('user_list', Object.values(users)); // Update all users
+        socket.broadcast.emit('user connected', `${nickname} has joined`);
+    });
+
+    // Handle chat messages
+    socket.on('chat_message', (message) => {
+        socket.broadcast.emit('chat_message', { nickname: users[socket.id], message });
+    });
+
+    // Handle "user is typing" notification
+    socket.on('typing', () => {
+        socket.broadcast.emit('user_typing', users[socket.id]);
+    });
+
+    // Handle user disconnection
+    socket.on('disconnect', () => {
+        if (users[socket.id]) {
+            io.emit('user disconnected', `${users[socket.id]} has left`);
+            delete users[socket.id]; // Remove from user list
+            io.emit('user_list', Object.values(users)); // Update all users
+        }
+        console.log('A user disconnected');
+    });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on http://localhost:3000');
 });
